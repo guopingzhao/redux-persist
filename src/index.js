ce6b458353492a79dc22e38772f0f4869565df6b
zhaoguoping <zhaoguoping@9lead.com>
2017-10-30 15:00:00 +0800

    保存初始化数据

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import storages from "./storage"
+const persistInit = "@@/persist"
 export default function persist(config={}) {
   const {
     ondispatch,
@@ -15,18 +16,36 @@ export default function persist(config={}) {
     ondispatch
   }
   return (createStore) => (a, b=JSON.parse(storage.getItem(key)) || undefined, c) => {
-    let store = createStore(a, b, c)
+    persist.initialState = createStore(a).getState()
+    function reducers (state, action, ...argv) {
+      if (action && action.type === persistInit) {
+        return a(action.newState, action, ...argv)
+      } else {
+        return a(state, action, ...argv)
+      }
+    }
+    let store = createStore(reducers, b, c)
     let _dispatch = store.dispatch
     store.dispatch = (action) => {
-      if (ondispatch) ondispatch({
-        action,
-        persistConfig: {
-          key,
-          storage
+      if (ondispatch) {
+        let newState = ondispatch({
+          action,
+          persistConfig: {
+            key,
+            storage
+          },
+          initialState: {...persist.initialState}
+        })
+        if (newState) {
+          _dispatch({
+            type: persistInit,
+            newState: Object.assign(JSON.parse(storage.getItem(key)), newState)
+          })
         }
-      })
+      }
       return _dispatch(action)
     }
+    store.clearPersist = () => storage.removeItem(key)
     store.subscribe(() => {
       save(key, storage, reducer(store.getState()))
     })
